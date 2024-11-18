535ea252a1732da9adabe5c6a471cbf32b88d527
injamamulhak0804 <injamamulhak080404@gmail.com>
2024-11-18 17:15:13 +0530

    feat:Add_lazy_loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import { Route, Routes } from "react-router-dom"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Project from "./components/Project"
-import Contact from "./components/Contact"
-function App() {
+import { lazy, Suspense } from "react"
+const Hero = lazy(()=>import('./components/Hero'))
+const Footer = lazy(()=>import('./components/Footer'))
+const Header = lazy(()=>import('./components/Header'))
+const About = lazy(()=>import('./components/About'))
+const Project = lazy(()=>import('./components/Project'))
+const Contact = lazy(()=>import('./components/Contact'))
 
+
+function App() {
   return (
     <>
       <div className="bg-[#011627] w-full relative overflow-hidden h-screen md:h-screen">
-        <Header/>
-          <Routes>
-            <Route path="/" element={ <Hero/>} />
-            <Route path="/about" element={ <About/>} />
-            <Route path="/project" element={ <Project/>} />
-            <Route path="/contact" element={ <Contact />} />
+        <Suspense><Header/></Suspense>
+          <Routes> 
+            <Route path="/" element={ <Suspense><Hero/></Suspense>}/>
+  
+            <Route path="/about" element={ <Suspense><About/></Suspense>} />
+            <Route path="/project" element={ <Suspense><Project/></Suspense>} />
+            <Route path="/contact" element={ <Suspense><Contact /></Suspense>} />
           </Routes>
-        <Footer/>
+        <Suspense><Footer/></Suspense>
       </div>
     </>
   )
