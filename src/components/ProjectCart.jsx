817d0f7bd9bff8f19345b958275456bdd63fafae
injamamulhak0804 <injamamulhak080404@gmail.com>
2026-05-20 06:03:45 +0530

    refactor: improve code formatting and consistency in ProjectCart component

--- src/components/ProjectCart.jsx
+++ src/components/ProjectCart.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { FIGMA } from '../utils/constants'
-import { FaFigma } from 'react-icons/fa'
+import React from "react";
+import { FIGMA } from "../utils/constants";
+import { FaFigma } from "react-icons/fa";
 
-const ProjectCart = ({data, icon}) => {
-    const {name, img, link, desc} = data
-    // console.log(data);
-    
+const ProjectCart = ({ data, icon }) => {
+  const { name, img, link, desc } = data;
+  // console.log(data);
+
   return (
-            <div  className='2xl:w-[350px] 2xl:h-[290px] md:w-[250px] mb-7 md:m-0 relative border flex-col h-[300px] w-[350px] md:h-[230px] justify-between flex rounded-lg border-[#1E2D3D]'>
-             <div className='bg-[#86E1F9] text-lg p-[0.15rem] rounded-sm top-2 right-2 absolute text-black'>
-              {icon}
-             </div>
-                {/* <FaFigma className='bg-[#86E1F9] text-2xl p-[0.25rem] rounded-sm top-2 right-2 absolute text-black'/> */}
-                    <img src={img} className='object-cover rounded-t-xl w-[100%]' alt="" />
-                    <div className='px-3'>
-                      <p className='pb-3 text-[#f0f0f0d8]'>{name} - {desc + " "} </p>
-                      <button className='px-3 py-2 bg-[#1C2B3A] mb-2 text-white text-xs font-fira  rounded-md'>
-                        <a href={link} target='_blank'>view-project</a>
-                      </button>
-                    </div>  
-              </div>
-  )
-}
+    <div className="2xl:w-[350px] 2xl:h-[290px] md:w-[250px] mb-0 md:m-0 relative border flex-col h-[300px] w-[350px] md:h-[230px] justify-between flex rounded-lg border-[#1E2D3D]">
+      <div className="bg-[#86E1F9] text-lg p-[0.15rem] rounded-sm top-2 right-2 absolute text-black">
+        {icon}
+      </div>
+      {/* <FaFigma className='bg-[#86E1F9] text-2xl p-[0.25rem] rounded-sm top-2 right-2 absolute text-black'/> */}
+      <img src={img} className="object-cover rounded-t-xl w-[100%]" alt="" />
+      <div className="px-3">
+        <p className="pb-3 text-[#f0f0f0d8]">{name} </p>
+        <button className="px-3 py-2 bg-[#1C2B3A] mb-2 text-white text-xs font-fira  rounded-md">
+          <a href={link} target="_blank">
+            view-project
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectCart
+export default ProjectCart;
